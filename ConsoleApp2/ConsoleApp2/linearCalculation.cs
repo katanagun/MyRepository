af2598d17d0d5c292c751d1f4cf679d34ae7215f
katanagun <cormack567@mail.ru>
2021-11-25 18:07:00 +0300

    lab2

---
+++ ConsoleApp2/ConsoleApp2/linearCalculation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bogdan
+{
+    public class linearCalculation
+    {
+        protected List<double> x;
+
+        public List<double> linearEquation(double a, double b)
+        {
+
+            if (b == 0)
+            {
+                return null;
+            }
+
+            //  x = -b / a;
+            return x = new List<double>() { -b / a };
+        }
+    }
+}
