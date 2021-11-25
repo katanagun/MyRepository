9257b91fc6d42269c7c6923135d79d17c120fa47
katanagun <cormack567@mail.ru>
2021-11-25 18:08:46 +0300

    lab4

---
+++ ConsoleApp4/ConsoleApp4/Bogdan/LinearCalculation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bogdan
+{
+    public class LinearCalculation
+    {
+        protected List<double> x;
+
+        public List<double> LinearEquation(double a, double b)
+        {
+
+            if (b == 0)
+            {
+                return null;
+            }
+
+            //  x = -b / a;
+            Console.WriteLine("Это линейное уравнение");
+            return x = new List<double>() { -b / a };
+        }
+    }
+}
---
+++ ConsoleApp4/ConsoleApp4/core/EquationInterface.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace core
+{
+    interface EquationInterface
+    {
+        List<double> solve(double a, double b, double c);
+        
+
+        
+    }
+}
